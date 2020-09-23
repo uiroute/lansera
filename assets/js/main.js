0a20995415ee5d53b03768c1690725cde8eb2896
davidbanu <davidbanu@ymail.com>
2020-09-23 12:20:26 +0300

    Dropdown light/dark theme implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 // Main.js
+
+if(localStorage){
+    console.log(localStorage.getItem('theme'));
+    if(localStorage.getItem('theme') === 'dark') {
+        $(".page-container").addClass("dark-theme");
+    }
+}
+
+// $(".page-container").addClass("dark-theme");
+$("#theme-select").on("click", function() {
+    let select = $( "#theme-select" ).val();
+    console.log(select)
+    if (select === 'dark') {
+        $(".page-container").addClass("dark-theme");
+        localStorage.setItem('theme', select);
+    }
+    if (select === 'light') {
+        $(".page-container").removeClass("dark-theme");
+        localStorage.setItem('theme', select)
+    }
+})
