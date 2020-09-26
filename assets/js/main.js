c80ac2b95caa694706de7df8b0ea55dbc42238e9
davidbanu <davidbanu@ymail.com>
2020-09-26 19:09:10 +0300

    Removing window reference from light/dark switch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 // Main.js
 
-if(window.localStorage){
-    if(window.localStorage.getItem('theme') === 'dark') {
+if(localStorage){
+    if(localStorage.getItem('theme') === 'dark') {
         $("#theme-select").val("dark");
         $(".page-container").addClass("dark-theme");
 
     }
-    if(window.localStorage.getItem('theme') === 'light') {
+    if(localStorage.getItem('theme') === 'light') {
         $("#theme-select").val("light");
         $(".page-container").removeClass("dark-theme");
     }
@@ -17,10 +17,10 @@ $("#theme-select").on("click", function() {
     let select = $( "#theme-select" ).val();
     if (select === 'dark') {
         $(".page-container").addClass("dark-theme");
-        window.localStorage.setItem('theme', select);
+        localStorage.setItem('theme', select);
     }
     if (select === 'light') {
         $(".page-container").removeClass("dark-theme");
-        window.localStorage.setItem('theme', select)
+        localStorage.setItem('theme', select)
     }
 })
